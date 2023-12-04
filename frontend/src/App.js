4712cecc365ece3c15aa37e4cef597bc824542ab
Joel Daniel Arguello Espinosa <jdarguello1@espe.edu.ec>
2023-12-03 20:26:00 -0500

    first_up

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header>
+        {/* Contenido del encabezado */}
+        <h1>Sistema de Gestión de Proyectos de Investigación</h1>
+      </header>
+      <main>
+        {/* Contenido principal de la página */}
+        <p>¡Bienvenido! Aquí podrás gestionar la información de los proyectos de investigación.</p>
+        {/* Agrega componentes adicionales según sea necesario */}
+      </main>
+      <footer>
+        {/* Contenido del pie de página */}
+        <p>© 2023 Universidad XYZ</p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
